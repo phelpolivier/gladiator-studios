74d855789e227697b9ba2df2fdf4422ca9cef64c
phelpolivier <phelpolivier7@gmail.com>
2024-06-14 12:17:51 -0300

    vs1.0.2 (Mobile Uptade)

---
+++ script/script.js
@@ -0,0 +1,17 @@
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+        pessoa.style.display = 'none'
+    }
+}
+
+function tamanho() {
+    if (window.innerWidth >= 800) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+        pessoa.style.display = 'none'
+    }
+}
